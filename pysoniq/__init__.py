851cb02028cc66923e93911c9be492d3cc5059a5
ume <laelume@gmail.com>
2025-12-12 09:49:52 +1300

    Added fourier methods

--- pysoniq/__init__.py
+++ pysoniq/__init__.py
@@ -3,20 +3,28 @@
 from .play import play
 from .stop import stop
 from .pause import pause, resume, is_paused
-from .loop import set_loop, is_looping
 from .io import load, save
+from .loop import set_loop, is_looping
+
 from .gain import (
     set_gain, get_gain, 
     set_volume_db, get_volume_db,
     adjust_gain_level, normalize, compress, limiter,
     db_to_linear, linear_to_db
 )
+from .fourier_stuff import stft, fft_frequencies, frames_to_time, amplitude_to_db
 
 __version__ = '0.1.0'
 __all__ = [
-    'play', 'stop', 'pause', 'resume', 'is_paused',
-    'load', 'save', 'set_loop', 'is_looping',
+    'play', 'stop', 
+    'pause', 'resume', 'is_paused',
+    'load', 'save', 
+    'set_loop', 'is_looping',
+    
     'set_gain', 'get_gain', 'set_volume_db', 'get_volume_db',
     'adjust_gain_level', 'normalize', 'compress', 'limiter',
-    'db_to_linear', 'linear_to_db'
+    'db_to_linear', 'linear_to_db', 
+
+    'stft', 'fft_frequencies', 'frames_to_time', 'amplitude_to_db'
 ]
+
