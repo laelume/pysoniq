54654aeafa81f3efa1cf5b9f16997441d87f965b
laelume <laelume@gmail.com>
2026-06-27 19:24:50 +0300

    Add Sphinx docs source (conf.py, index.rst) to pysoniq repo

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = "pysoniq"
+author = "laelume"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+html_theme = "alabaster"
+
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+pysoniq
+=======
+
+Lightweight Pythonic cross-platform audio playback library.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: API
+
+   api/modules
